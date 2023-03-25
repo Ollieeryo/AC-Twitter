f48dba76d6c81ec8166064ae1bb9c8b8361192f0
Felizlin94 <felizlin94@gmail.com>
2023-03-26 00:51:00 +0800

    feat: TweetModal, ReplyModal, ReplyPost, ReplyItem

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -1,21 +1,45 @@
 import styles from './MainSection.module.scss';
+
 import Header from '../Header/Header';
-import TweetItem from '../TweetItem/TweetItem';
-import TweetInput from '../TweetInput/TweetInput';
+// import TweetItem from '../TweetItem/TweetItem';
+// import TweetInput from '../TweetInput/TweetInput';
+import ReplyItem from '../ReplyItem/ReplyItem';
+import ReplyPost from '../ReplyPost/ReplyPost';
 
 function MainSection() {
 	return (
 		<div className={styles.container}>
-			<Header content='首頁' />
-
-			<TweetInput />
-
-			<TweetItem />
-			<TweetItem />
-			<TweetItem />
-			<TweetItem />
-			<TweetItem />
+			{/* <Header content='首頁' />
+			<HomePage /> */}
+			<Header content='推文' />
+			<ReplyPage />
 		</div>
+	);
+}
+
+// function HomePage() {
+// 	return (
+// 		<>
+// 			<TweetInput />
+// 			<TweetItem />
+// 			<TweetItem />
+// 			<TweetItem />
+// 			<TweetItem />
+// 			<TweetItem />
+// 		</>
+// 	);
+// }
+
+function ReplyPage() {
+	return (
+		<>
+			<ReplyPost />
+			<ReplyItem />
+			<ReplyItem />
+			<ReplyItem />
+			<ReplyItem />
+			<ReplyItem />
+		</>
 	);
 }
 
